6e2e454751a3b2a3086dae17a534f5fad8f2db0d
Drewjoe <andrewjose711@gmail.com>
2022-05-21 17:08:59 -0700

    Code passes betty check

--- sum.c
+++ sum.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 
+/**
+ * main - writes to stdout
+ * void: no arg passed
+ * Return: Aways 0
+ */
 int main(void)
 {
-    write(2, "\n ALX Task 0 file\n", 18);
-    return 1;
+	write(2, "ALX Task 0 file\n", 18);
+	return (1);
 }
